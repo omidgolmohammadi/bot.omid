13dc5ded633f2a413bf94239fe21fb4629184c3d
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-16 15:07:17 +0430

    Added files via upload

---
+++ config.lua
@@ -0,0 +1,53 @@
+do local _ = {
+  disabled_channels = {},
+  enabled_plugins = {
+    "Echo",
+    "Get",
+    "google",
+    "gps",
+    "location",
+    "Set",
+    "Link",
+    "AntiLink",
+    "Chat",
+    "channelmod",
+    "Plugins",
+    "Broadcast",
+    "Welcome",
+    "Wai",
+    "Tagall",
+    "Term",
+    "Spammer",
+    "Media",
+    "Info",
+    "Filtering",
+    "FileManager",
+    "Feedback",
+    "Realm",
+    "Gpmod",
+    "invite",
+    "anti-flood",
+    "channelmod",
+    "Send",
+    "T2i",
+    "AntiChat",
+    "Cleaner",
+    "Id",
+    "weather",
+    "Stats",
+    "Images",
+    "Kickme",
+    "Insudo",
+    "Support",
+    "Invite",
+    "AntiTag"
+  },
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    164565909
+  }
+}
+return _
+end
